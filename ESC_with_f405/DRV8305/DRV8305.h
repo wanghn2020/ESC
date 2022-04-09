8d9fddcc9e6261cfb01f5d61b729d77111bba532
wanghn2020 <63180961+wanghn2020@users.noreply.github.com>
2022-04-09 21:34:38 +0800

    Add files via upload

--- ESC_with_f405/DRV8305/DRV8305.h
+++ ESC_with_f405/DRV8305/DRV8305.h
@@ -1,3 +1,11 @@
+#include "main.h"
+
+#define   HS_Gate_Drive_Control_REG	0xb8
+
+void DRV8305_Init(void);
+void Clear_DRV8305_Fault(void);
+void Read_DRV8305_REG(void);
+void Phase_Align(void);
+void Phase_Stop(void);
 
 
-void ClearFault(void);
