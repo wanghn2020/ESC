cb9172dfc0cc412b5da020ab8a6695fa8106e23f
wanghn2020 <63180961+wanghn2020@users.noreply.github.com>
2022-04-10 17:45:36 +0800

    Add files via upload

--- ESC_with_f405/DRV8305/DRV8305.h
+++ ESC_with_f405/DRV8305/DRV8305.h
@@ -4,14 +4,20 @@
 
 void DRV8305_Init(void);
 void Clear_DRV8305_Fault(void);
-void Read_DRV8305_REG(void);
+void Read_DRV8305_REG(uint8_t Addr);
 void Phase_Align(void);
 void Phase1_AB(void);
+void Phase12_AB_CB(void);
 void Phase2_CB(void);
+void Phase23_CB_CA(void);
 void Phase3_CA(void);
+void Phase34_CA_BA(void);
 void Phase4_BA(void);
+void Phase45_BC_AC(void);
 void Phase5_BC(void);
+void Phase56_AC_AB(void);
 void Phase6_AC(void);
+void Phase7_AB(void);
 void Phase_Stop(void);
 
-
+void Delay(__IO uint32_t nCount);
